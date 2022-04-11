aeb8698fc01ead4b15cbbc0c4466e5024ae26cfc
matheusPascoal <pascoalleander@gmail.com>
2022-04-11 18:02:55 -0300

    add pages music e imc

---
+++ lib/pages/calculadora_imc.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CalculadoraIMC extends StatefulWidget {
+  const CalculadoraIMC({Key? key}) : super(key: key);
+
+  @override
+  State<CalculadoraIMC> createState() => _CalculadoraIMCState();
+}
+
+class _CalculadoraIMCState extends State<CalculadoraIMC> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/pages/music_player.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MusicPLayer extends StatefulWidget {
+  const MusicPLayer({Key? key}) : super(key: key);
+
+  @override
+  State<MusicPLayer> createState() => _MusicPLayerState();
+}
+
+class _MusicPLayerState extends State<MusicPLayer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
